c29e84c13f3ff009b19a22b109b16f26e4589362
Lucas Carozza <lucas.carozza@icloud.com>
2025-05-03 12:04:14 -0300

    Setup Prisma

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,20 @@
 // External libraries
 import fastify from "fastify";
 
+/*
+ * Ensure all dependencies are installed,
+ * then execute `npx prisma generate` in the terminal
+ * to initialize PrismaClient.
+ */
+import { PrismaClient } from "generated/prisma/client";
+
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Usuário Teste",
+    email: "email@email.com",
+  },
+});
